76b394d93982beb8ab971680d274c9f0f41228e3
El hadji youssou Drame <youssou18drame@gmail.com>
2026-07-29 03:07:02 +0200

    Fix final CORS headers

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }))
 
 app.use(express.json())
