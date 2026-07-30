677ec9b93bce5eeb4fc4fc8fe1e01a00bc7c4808
El hadji youssou Drame <youssou18drame@gmail.com>
2026-07-30 22:49:07 +0200

    fix vercel prisma backend

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 const express = require('express')
 const cors = require('cors')
-
 const route = require('./routes/index')
 
 const app = express()
 
 app.use(cors({
-  origin: '*',
-  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
-  allowedHeaders: [
-    'Content-Type',
-    'Authorization'
-  ]
+  origin: '*'
 }))
 
 app.use(express.json())
