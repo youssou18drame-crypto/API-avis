11b5cd602daa1bca6f37b7456a0624ce01ed54e9
El hadji youssou Drame <youssou18drame@gmail.com>
2026-07-30 22:41:38 +0200

    fix database config

--- index.js
+++ index.js
@@ -1,36 +1,21 @@
-const express = require("express");
-const cors = require("cors");
-const route = require("./routes/index");
-const prisma = require("./lib/prisma");
+const express = require('express')
+const cors = require('cors')
 
-const app = express();
+const route = require('./routes/index')
+
+const app = express()
 
 app.use(cors({
-  origin: [
-    "https://app-sio-frontend.vercel.app",
-    "http://localhost:3000"
-  ],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+  origin: '*',
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  allowedHeaders: [
+    'Content-Type',
+    'Authorization'
+  ]
+}))
 
-app.use(express.json());
+app.use(express.json())
 
-app.use("/", route);
+app.use('/', route)
 
-if (process.env.NODE_ENV !== "production") {
-  const server = app.listen(4000, () => {
-    console.log("Server running on http://localhost:4000");
-  });
-
-  const shutdown = async () => {
-    await prisma.$disconnect();
-    server.close(() => process.exit(0));
-  };
-
-  process.on("SIGINT", shutdown);
-  process.on("SIGTERM", shutdown);
-}
-
-module.exports = app;
+module.exports = app
